249f7c1ca26f7772b8f4e5d720ac0246aff60aa4
Helenia Mungu-Arere <hm4945@princeton.edu>
2026-06-01 15:12:33 -0700

    debugging parallel processing

--- public/backend/app.py
+++ public/backend/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 from aggregator import search_google_books, search_open_library, combine_data
+from concurrent.futures import ThreadPoolExecutor
 
 app = Flask(__name__)
 CORS(app)
@@ -17,8 +18,13 @@ def search_books():
     if not title:
         return jsonify([])
 
-    g_results = search_google_books(title, author)
-    o_results = search_open_library(title, author)
+    with ThreadPoolExecutor() as executor:
+        future_g = executor.submit(search_google_books, title, author)
+        future_o = executor.submit(search_open_library, title, author)
+
+        g_results = future_g.result()
+        o_results = future_o.result()
+
     result = combine_data(g_results, o_results)
     return jsonify(result)
 
